9819ed391dfb89bd75484cfe3d4f43dc93303ac7
tester1 <manish.qatesterje@gmail.com>
2024-11-12 17:40:20 +0530

    learn 12 nov

--- tests/checkbox.spec.js
+++ tests/checkbox.spec.js
@@ -9,17 +9,21 @@ test("test1", async ({ page }) => {
     expect(await page.locator("(//i[@class='a-icon a-icon-checkbox'])[8]").isChecked()).toBeFalsy();
     const options = await page.$$('ul>li label')
     
+    
     for (let option of options) {
         const value = await option.textContent();
+        console.log(value);
         if (await value.includes('OnePlus')) {
             await option.click();
             break;
         }
     }
+    await page.pause();
 
      await page.locator("div[class='s-widget-container s-spacing-small s-widget-container-height-small celwidget slot=MAIN template=SEARCH_RESULTS widgetId=search-results_3'] span[class='a-size-base-plus a-color-base a-text-normal']").click();
     //  await page.pause();
-     const newPage = await new Promise(resolve => page.once('popup', resolve));
+    const newPage = await page.waitForEvent('popup');
+    //  const newPage = await new Promise(resolve => page.once('popup', resolve));
     
     // await page.goto("https://www.amazon.in/OnePlus-Super-Silver-128GB-Storage/dp/B0D5YD7QSS/ref=sr_1_3?dib=eyJ2IjoiMSJ9.l4TLOasiVag3HcSLBjLH6LXxlQvyEVG2OESItNsld7MGUkHCUS5AoCoGOnKENLa1jz6YywHbb6ter6YTq1-2OKvGDEwVdm35DuPVJeeVZFpXbpuQp1r_HGnnz65bjhLlNHdkXA6ki9EuyND5WpWXXUc6684Y_JW4p8SmFtPsW0hOr6AQJ0Vm1gnAXkGTXZrnmRP6IcbWqfZ8Cx1yvqP4hKyo8GvJytaTOBMfPC5H5KyFThvc2UQgDMgGU6d2R_Gk-tM1I7s_TkkC04plJOLyA9_s8XIeKFrQidGn4pMoIIM._VokKPJX0AlsxIwU2Piz2dnLjn0t5cBrqOvTjHLld8I&dib_tag=se&qid=1721820494&refinements=p_89%3AOnePlus&rnid=3837712031&s=electronics&sr=1-3&th=1")
     //  await page.waitForTimeout(5000);
--- tests/fileupload.spec.js
+++ tests/fileupload.spec.js
@@ -7,6 +7,7 @@ test('upload file',async ({page})=>{
     await page.goto("https://davidwalsh.name/demo/multiple-file-upload.php");
     // await page.locator('#filesToUpload').setInputFiles("filefolder/file1.txt"); //for single file upload
     await page.locator('#filesToUpload').setInputFiles(["filefolder/file1.txt","filefolder/file2.xls"]);   //for multiple file upload
+    await page.locator('#filesToUpload').setInputFiles([]);   // for remove files
     await page.pause();
  })
 
